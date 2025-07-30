ed94930686bacbad4aaa7866e805df6a72ca7445
ChinJoshi <cshjoshi@gmail.com>
2025-07-30 18:26:03 -0500

    remove typo on home page

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def landing_page():
     return HTMLResponse(content = """<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">jkjhjk
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Algorithms API</title>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
