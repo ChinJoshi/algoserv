b9ffddbad3bc2f7f6e3efa4283ab93d06dedf70e
ChinJoshi <cshjoshi@gmail.com>
2026-01-13 17:35:39 -0800

    reformatted code for improved readability and consistency

--- app/routers/leaf_similar_trees.py
+++ app/routers/leaf_similar_trees.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
 from collections import deque
+
+from fastapi import APIRouter
+
 router = APIRouter()
 
 
@@ -10,20 +12,25 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 def array_to_tree(arr):
     # Helper function to construct the tree
     def helper(i):
         if i >= len(arr) or arr[i] is None:
             return None
         node = TreeNode(arr[i])
-        node.left = helper(2*i + 1)
-        node.right = helper(2*i + 2)
+        node.left = helper(2 * i + 1)
+        node.right = helper(2 * i + 2)
         return node
-    
+
     return helper(0)
 
 
-@router.post("/leaf-similar-trees", summary="Leaf Similar Trees", description="Time Complexity O(V+E). Space Complexity O(V). Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence. Two binary trees are considered leaf-similar if their leaf value sequence is the same. \n\n binary trees should be input in array form")
+@router.post(
+    "/leaf-similar-trees",
+    summary="Leaf Similar Trees",
+    description="Time Complexity O(V+E). Space Complexity O(V). Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence. Two binary trees are considered leaf-similar if their leaf value sequence is the same. \n\n binary trees should be input in array form",
+)
 def leafSimilar(root1: list[int], root2: list[int]) -> bool:
     root1 = array_to_tree(root1)
     root2 = array_to_tree(root2)
@@ -44,7 +51,7 @@ def leafSimilar(root1: list[int], root2: list[int]) -> bool:
                 tree_leaves.append(node.val)
         # print(tree_leaves)
         return tree_leaves
-    
+
     tree1_leaves = dfs_get_leaves(root1)
     tree2_leaves = dfs_get_leaves(root2)
 
