3f8c3434a87ae264680e3d506fe77c558062c9e1
ChinJoshi <cshjoshi@gmail.com>
2026-01-20 14:15:28 -0800

    hw

--- my_app/main.py
+++ my_app/main.py
@@ -51,7 +51,7 @@ def landing_page():
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Algorithms API</title>
+  <title>Algorithms API Hello World</title>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body class="bg-gradient-to-br from-indigo-50 to-blue-100 min-h-screen">
